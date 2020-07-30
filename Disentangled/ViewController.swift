dd22c795fe456d9fcd00c581d3d0f06d8f160f92
melaniesky <melanie.jackson3181@gmail.com>
2020-07-30 19:22:56 -0400

    version 7/30/2020 7:22

--- Disentangled/ViewController.swift
+++ Disentangled/ViewController.swift
@@ -10,6 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var title1: UILabel!
+    
+    
+    @IBOutlet weak var paragraph1: UILabel!
+    
+    @IBAction func button1(_ sender: UIButton) {
+    }
+    
+    @IBAction func button2(_ sender: UIButton) {
+    }
+    
+    @IBAction func button3(_ sender: UIButton) {
+    }
+    
+    @IBAction func button4(_ sender: UIButton) {
+    }
+    
+    @IBAction func button5(_ sender: UIButton) {
+    }
+    
+    @IBAction func button6(_ sender: UIButton) {
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
